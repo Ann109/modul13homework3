99d5d4400556a1bd4b32bea6bcede2694b363850
Ann109 <andersonhloa@gmail.com>
2024-12-29 00:52:08 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,25 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message
+import asyncio
+
+bot = Bot(token='7476944299:AAGoWkqfCfJBwLLDrMQP8m8FL1pmdKmycD0')
+dp = Dispatcher()
+
+
+@dp.message()
+async def start_message(message: Message):
+        await message.answer('Введите команду /start, чтобы начать общение.')
+
+
+@dp.message(Command('/start'))
+async def all_messages(message: Message):
+        await message.answer('Привет! Я бот помогающий твоему здоровью.')
+        print('Привет! Я бот помогающий твоему здоровью.')
+
+
+async def main():
+        await dp.start_polling(bot)
+
+
+asyncio.run(main())
